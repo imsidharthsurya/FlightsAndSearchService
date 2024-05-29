9395f6c345c94ebfee2565555ab982538cb6371e
Sidharth Surya <imsidharthsurya@gmail.com>
2024-05-29 11:39:01 +0530

    initial server commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express=require("express");
+
+const setupAndStartServer=async()=>{
+    const app=express();
+    const PORT=3000;
+
+    app.listen(PORT,()=>{
+        console.log(`server is running on port ${PORT}`);
+    })
+}
+
+setupAndStartServer();
