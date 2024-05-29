d5c7cedf97ecafb71d652745af83842ba450548c
Sidharth Surya <imsidharthsurya@gmail.com>
2024-05-29 20:38:50 +0530

    models & migrations created also city-repo for creating & deleting city

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 
 const {PORT}=require("./config/serverConfig")
+const CityRepository=require("./repository/city-repository")
 
 const setupAndStartServer=async()=>{
     const app=express();
@@ -8,8 +9,13 @@ const setupAndStartServer=async()=>{
 
     app.use(express.json())
 
-    app.listen(PORT,()=>{
+    app.listen(PORT,async()=>{
+
         console.log(`server is running on port ${PORT}`);
+        const city=new CityRepository();
+        // city.createCity({name:"New Delhi"})
+        city.deleteCity(1);
+
     })
 }
 
