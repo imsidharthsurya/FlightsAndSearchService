07406bfcb375eb71c0a3b7e5674cf25994c52234
Sidharth Surya <imsidharthsurya@gmail.com>
2024-05-29 15:18:54 +0530

    sequelize init and movied models inside src folder

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const setupAndStartServer=async()=>{
     const app=express();
     // const PORT=3000;
 
+    app.use(express.json())
+
     app.listen(PORT,()=>{
         console.log(`server is running on port ${PORT}`);
     })
