331725535fcf13b38a74c29f52c9cb037020c16e
Sidharth Surya <imsidharthsurya@gmail.com>
2024-05-31 23:32:22 +0530

    db synchronization & env added for synchronization

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const {PORT}=require("./config/serverConfig")
 // const CityRepository=require("./repository/city-repository")
 const apiRoutes=require("./routes/index")
 
+// const {Airport,City}=require("./models/index")
+const db=require("./models/index")
+const {City,Airport}=require("./models/index")
 const setupAndStartServer=async()=>{
     const app=express();
     // const PORT=3000;
@@ -15,14 +18,21 @@ const setupAndStartServer=async()=>{
     app.listen(PORT,async()=>{
 
         console.log(`server is running on port ${PORT}`);
-        // const city=new CityRepository();
-        // city.createCity({name:"New Delhi"})
-        // city.deleteCity(3)
-        // city.updateCity(4,{name:"Sidharth nagar"});
-        // const cityData=await city.getCity(4)
-        // console.log(cityData)
+        if(process.env.SYNC_DB){
+            await db.sequelize.sync({alter:true})
+        }
         
-
+        // const city=await City.findOne({
+        //     where:{
+        //         id:14
+        //     }
+        // })
+        // const airports=await city.getAirports();
+        // await Airport.create({
+        //     name:"Darbhanga Airport",
+        //     cityId:4
+        // })
+        // console.log(airports)
     })
 }
 
